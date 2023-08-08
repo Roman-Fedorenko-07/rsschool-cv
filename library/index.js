78e36a6ad16fba6e27f7de2e91d2080dec753d09
Roman-Fedorenko-07 <rmn.fedorenko7@gmail.com>
2023-08-08 17:28:54 +0300

    fix: files

--- library/index.js
+++ library/index.js
@@ -1,14 +1,7 @@
 console.log('98/100\n-2 points becouse in section "About" absent transparent element size 26x26');
 
-const menu = document.querySelector('navig')
-const menuIcon = document.querySelector('hrNav')
-
-const body = document.body;
-
-if (menu && menuIcon) {
-    menuIcon.addEventListener('click', () => {
-        menu.classList.toggle('active')
-        menuIcon.classList.toggle('active')
-        body.classList.toggle('lock')
-    })
-} 
+const menuBtn = document.querySelector('.hrNav');
+const menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+})

944a9d2057d88b387df4e893baf0cf1745ff7a06
Roman-Fedorenko-07 <rmn.fedorenko7@gmail.com>
2023-08-10 17:06:09 +0300

    fix: files

--- library/index.js
+++ library/index.js
@@ -1,15 +1,38 @@
 console.log('98/100\n-2 points becouse in section "About" absent transparent element size 26x26');
 
-const menuBtn = document.querySelector('.hrNav');
 const menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-})
+const menuBtn = document.querySelector('.hrNav');
 
-document.addEventListener('click', (e) => {
-	const click = e.composedPath().includes(menu);
-	if ( !click ) {
-		menu.style.display = 'none'
+const body = document.body;
+
+if (menu && menuBtn) {
+	menuBtn.addEventListener('click', e => {
+		menu.classList.toggle('active')
+		menuBtn.classList.toggle('active')
+		body.classList.toggle('lock')
+	})
+	
+	menu.addEventListener('click', e => {
+	if(e.target.classList.contains('body')) {
+		menu.classList.remove('active')
+		menuBtn.classList.remove('active')
+		body.classList.remove('lock')
 	}
+	})
+
+	document.querySelector('main').addEventListener('click', e => {
+		if(e.target.classList.contains('body')) {
+			menu.classList.remove('active')
+			menuBtn.classList.remove('active')
+			body.classList.remove('lock')
+		}
+		})
+
+menu.querySelectorAll('.menuItem').forEach(link => {
+	link.addEventListener('click', () => {
+		menu.classList.remove('active')
+		menuBtn.classList.remove('active')
+		body.classList.remove('lock')
+	})
 })
+}

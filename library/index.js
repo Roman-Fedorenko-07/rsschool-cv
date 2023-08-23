7c8f39fe5167f071f833852504ad79aa1ca0d140
Roman-Fedorenko-07 <rmn.fedorenko7@gmail.com>
2023-08-23 18:20:59 +0300

    init: slides for all button on section 'About'

--- library/index.js
+++ library/index.js
@@ -37,27 +37,77 @@ menu.querySelectorAll('.menuItem').forEach(link => {
 })
 	
 }
+///////////////////////////////////////////
+const img1 = document.getElementById("flangImg1");
+const img2 = document.getElementById("CentrImg");
+const img3 = document.getElementById("flangImg2");
 
-let img1 = document.getElementById("flangImg1");
-let img2 = document.getElementById("CentrImg");
-let img3 = document.getElementById("flangImg2");
+const slides2 = document.getElementById("div-imgAbout2-2");
+const slides3 = document.getElementById("div-imgAbout3-2");
 
 
-let slides2 = document.getElementById("div-imgAbout2-2");
-let slides3 = document.getElementById("flangImg3-2");
 
-let dots2 = document.getElementById("CI2");
+const dots1 = document.getElementById("CI1");
+const dots2 = document.getElementById("CI2");
+const dots3 = document.getElementById("CI3");
+///////////////////////////////////////////
+
 
 dots2.addEventListener("click", function(event) {
 	if (!dots2) return console.log('lox');
 	else {
-	slides2.classList.toggle('slide1');	
-	img1.classList.toggle('slide1-img1');
-	img2.classList.toggle('slide1-img');
-	img3.classList.toggle('slide1-img');
+	slides2.classList.add('slide1');	
+	img1.classList.add('slide1-img1');
+	img2.classList.add('slide1-img');
+	img3.classList.add('slide1-img');
+
+	slides2.classList.remove('slide2-img');	
+	img3.classList.remove('slide2-img');
+	img2.classList.remove('slide2-img-2');
+	slides3.classList.remove('slide2');
 	}
 })
 
+dots3.addEventListener("click", function(event) {
+	if (!dots3) return console.log('lox');
+	else {
+		slides2.classList.remove('slide1');	
+		img2.classList.remove('slide1-img');
+		img3.classList.remove('slide1-img');
+		
+	slides2.classList.toggle('slide2-img');	
+	img3.classList.add('slide2-img');
+	img2.classList.add('slide2-img-2');
+	slides3.classList.add('slide2');
+	img1.classList.add('slide1-img1');
+	}
+})
+
+dots1.addEventListener("click", function(event) {
+	if (!dots3) return console.log('lox');
+	else {
+		slides2.classList.remove('slide1');	
+		img2.classList.remove('slide1-img');
+		img3.classList.remove('slide1-img');
+		
+	slides2.classList.remove('slide2-img');	
+	img3.classList.remove('slide2-img');
+	img2.classList.remove('slide2-img-2');
+	slides3.classList.remove('slide2');
+	img1.classList.remove('slide1-img1');
+	}
+})
+
+const prof = document.getElementsByClassName('proficon')
+const reg = document.getElementsByClassName('reg')
+
+prof.addEventListener("click", function(event) {
+	if (!prof) return console.log('lox');
+	else {
+	reg.classList.toggle('profOpen');
+	}
+}
+)
 
 
 
